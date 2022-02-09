19da09714cb3b525f8d28e5dba9cefc6d8a3fc57
Aayushh Kartik <aayush.kartik123@gmail.com>
2022-02-09 14:16:14 +0530

    New addded components

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return <div>
+ <footer class="container-fluid">
+  <p >Follow us on</p>
+  {/* <div class="copyright" style="font-size:13px; margin-top:5px"> */}
+   <p>Copyright  © <script type="text/javascript">var year = new Date();document.write(year.getFullYear());</script> </p>
+  {/* </div> */}
+</footer>
+  </div>;
+};
+
+export default Footer;
